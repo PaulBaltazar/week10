db850aaa0d9c3b85dbf7aa48e79103eb5cc4d24c
Paul <paulpetergb@hotmail.com>
2022-11-08 14:48:25 -0800

    while and for

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// for (let i = 0; 1 < 1000000; i++) {
+//     console.log(i);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(2 **i);
+}
+
+let i = 0
+while (i <= 10) {
+    console.log(2 **i); i++
+}
