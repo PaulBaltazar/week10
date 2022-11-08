508ea766fee0435e88da5b7fdf57858e8039ca12
Paul <paulpetergb@hotmail.com>
2022-11-08 15:15:18 -0800

    do/while activity

--- script/script.js
+++ script/script.js
@@ -2,11 +2,23 @@
 //     console.log(i);
 // }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(2 **i);
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(2 **i);
+// }
 
-let i = 0
-while (i <= 10) {
-    console.log(2 **i); i++
+// let i = 0
+// while (i <= 10) {
+//     console.log(2 **i); i++
+// }
+
+let input;
+do {
+    input = prompt("What is your name?");
+    //prompt always returns string
+    //convert input to number
+    console.log(Number(input));
 }
+while (!isNaN(Number(input)));
+
+// alert("Hello " + input);
+alert(`Hello ${input}`)
