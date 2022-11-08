fe66d51df16b05911c691e933072a1fdbd0cec20
Paul <paulpetergb@hotmail.com>
2022-11-08 15:40:54 -0800

    array methods

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // for (let i = 0; 1 < 1000000; i++) {
 //     console.log(i);
 // }
-
+// loop activity
 // for (let i = 1; i <= 10; i++) {
 //     console.log(2 **i);
 // }
@@ -11,14 +11,29 @@
 //     console.log(2 **i); i++
 // }
 
-let input;
-do {
-    input = prompt("What is your name?");
-    //prompt always returns string
-    //convert input to number
-    console.log(Number(input));
-}
-while (!isNaN(Number(input)));
+// let input;
+// do {
+//     input = prompt("What is your name?");
+//     //prompt always returns string
+//     //convert input to number
+//     console.log(Number(input));
+// }
+// while (!isNaN(Number(input)));
 
-// alert("Hello " + input);
-alert(`Hello ${input}`)
+// // alert("Hello " + input);
+// alert(`Hello ${input}`)
+
+let fruits = ['Apple','Banana'];
+fruits.push("Orange");
+console.log(fruits);
+
+fruits.pop;
+console.log(fruits);
+
+fruits.unshift("Mango");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+console.log(fruits.indexOf("Apple"))
